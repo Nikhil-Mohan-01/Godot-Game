34585c796e37976cb5a1f2e69cdd4d9b48b23fb3
Nikhil-Mohan-01 <127566613+Nikhil-Mohan-01@users.noreply.github.com>
2024-07-07 18:14:12 -0400

    Finished the map and added the mini map feature

--- SRC/TopPlayerTest1.gd
+++ SRC/TopPlayerTest1.gd
@@ -1,9 +1,23 @@
 extends CharacterBody2D
 
-
 const speed = 50
 
-var friction = true
+@onready var cam = $Camera2D
+var miniMap = false
+
+func _input(event):
+	if event is InputEventKey:
+		match event.keycode:
+			KEY_M:
+				miniMap = event.pressed
+				
 
 func _process(delta):
 	position += Input.get_vector("left", "right", "up", "down") * speed * delta
+	
+	if miniMap:
+		cam.global_position = Vector2(537, 238)
+		cam.zoom = Vector2(1.025, 0.865)
+	else:
+		cam.position = position
+		cam.zoom = Vector2.ONE * 7.875
